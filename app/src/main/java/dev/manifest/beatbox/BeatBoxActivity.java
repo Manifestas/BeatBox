96f47857d62334d097940bd9928f313e594dd41c
manifest <doslovno@gmail.com>
2018-12-28 14:11:43 +0300

    remove onCreate

--- app/src/main/java/dev/manifest/beatbox/BeatBoxActivity.java
+++ app/src/main/java/dev/manifest/beatbox/BeatBoxActivity.java
@@ -1,15 +1,8 @@
 package dev.manifest.beatbox;
 
 import android.support.v4.app.Fragment;
-import android.os.Bundle;
 
 public class BeatBoxActivity extends SingleFragmentActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_beat_box);
-    }
 
     @Override
     protected Fragment createFragment() {
