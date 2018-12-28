18d1c71f91402a575d93702bbb68422aec14450e
manifest <doslovno@gmail.com>
2018-12-28 17:42:23 +0300

    play sound on button click

--- app/src/main/java/dev/manifest/beatbox/SoundViewModel.java
+++ app/src/main/java/dev/manifest/beatbox/SoundViewModel.java
@@ -25,4 +25,8 @@ public class SoundViewModel extends BaseObservable {
         this.sound = sound;
         notifyChange();
     }
+
+    public void onButtonClicked() {
+        beatBox.play(sound);
+    }
 }
